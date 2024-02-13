a385afe22421a18d73e1aa920b5b5b7b1f66856d
cristofermateo <christocheter@gmail.com>
2024-02-13 17:22:58 -0600

    huha

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('chirps.index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'message' => 'required'
+        ]);
+
+
+
+        $mensaje = request('mensaje');
+        Chirp::create([
+            'message' => $request->get('message'),
+            'user_id' => auth()->id(),
+        ]);
+        return to_route('chirps.index')
+        ->with('status','chirp created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
